bbf956ba605ae923b91986449c3b2aae72ad1004
gorhkd <gorhkd223@gmail.com>
2024-11-19 18:18:58 +0900

    ë¦delete part.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,14 +20,12 @@ public class Main {
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
-            System.out.println("입력된 명령어 : %s".formatted(cmd));
+            System.out.println("입력된 명령어 : " + cmd);
 
             if (cmd.equals("종료")) {
                 System.out.println("== 명령 앱 종료 ==");
                 break;
-            }
-
-            else if (cmd.equals("등록")) {
+            } else if (cmd.equals("등록")) {
                 System.out.println("명언 : ");
                 String wise_saying = scanner.nextLine().trim();
 
@@ -39,9 +37,7 @@ public class Main {
                 System.out.println(nextId + "번 명언이 등록되었습니다.");
 
                 nextId++;
-            }
-
-            else if (cmd.equals("목록")) {
+            } else if (cmd.equals("목록")) {
                 if (quotes.isEmpty()) {
                     System.out.println("등록된 명언이 없습니다.");
                 } else {
@@ -55,34 +51,29 @@ public class Main {
                 System.out.println("삭제 할 번호를 알려주세요.");
                 int delete_id = scanner.nextInt();
                 scanner.nextLine();
-
                 boolean found = false;
 
-                Iterator<Information> iterator = quotes.iterator();
-
-                while (iterator.hasNext()){
-                    Information quote = iterator.next();
-                    if (quote.id == delete_id) {
-                        iterator.remove();
-                        System.out.println(delete_id + "번 명언이 삭제되었습니다.");
-                        found = true;
-                        break;
+                for(Information q : quotes) {
+                    if(delete_id == q.id){
+                    quotes.removeIf(quote -> quote.id == delete_id);
+                    System.out.println(delete_id + "번 명언이 삭제되었습니다.");
+                    found = true;
+                    break;
                     }
-                }
-                if(!found) {
+                } if (!found) {
                     System.out.println(delete_id + "번 명언은 존재 하지 않습니다.");
                 }
-                }
+            }
 
 
-               else {
+            else {
                 System.out.println("알 수 없는 명령어입니다.");
             }
 
         }
-
-        scanner.close();
     }
+
+
 
 
 
@@ -104,7 +95,6 @@ public class Main {
                     "작가 : " + author + '\'' +
                     "명언 : " + wise_saying;
         }
-
 
     }
 
